a7e45eb2165092e7665cda2dbfb162482d8fa34a
Rodolpho <redsmedeiros@gmail.com>
2023-05-20 10:15:30 -0300

    primeiro

---
+++ src/main/java/com/gestorApp/GestorAppApplication.java
@@ -0,0 +1,13 @@
+package com.gestorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestorApp/GestorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
